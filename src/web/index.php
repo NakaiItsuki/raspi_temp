87618ab4ebaec843813a5e38b2fa4c71faa104f4
Nakai Itsuki <nakai.itsuki.ul@tut.jp>
2023-09-21 22:25:29 +0900

    edit index.php

--- src/web/index.php
+++ src/web/index.php
@@ -1,36 +1,58 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <title>BME280 温度・湿度・気圧</title>
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="preconnect" href="https://fonts.gstatic.com">
-  <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100;400;600;800&display=swap" rel="stylesheet">
-  <link rel="stylesheet" href="./style.css">
+    <title>BME280 温度・湿度・気圧</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100;400;600;800&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="./style.css">
+    <script src="chart.min.js"></script>
+    <script src="jquery-3.5.1.min.js"></script>
 </head>
 <body>
 <?
 try {
   // MariaDB接続
 $pdo = new PDO (
-  'mysql:host=192.168.10.2;dbname=temp;charset=utf8mb4','piuser','Pi1qaz2wsx',
-  [
+    'mysql:host=192.168.10.2;dbname=temp;charset=utf8mb4','piuser','Pi1qaz2wsx',
+    [
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-  ]
+    ]
 );
 $stmt = $pdo->query("SELECT date, temp, humi, pres from thp");
 while ($row = $stmt->fetch()) {
-  print "$row[date], $row[temp], $row[humi], $row[pres] <br> \n";
+    $dates[] = $row[date];
+    $temps[] = $row[temp];
+    $humis[] = $row[humi];
+    $press[] = $row[pres];
 }
 // MariaDB切断
 $pdo = null;
 // エラー処理
 } catch (PDOException $e) {
-  echo $e->getMessage() . PHP_EOL;
-  exit;
+    echo $e->getMessage() . PHP_EOL;
+    exit;
 }
 ?>
+<script>
+    var dates = <?php echo $dates; ?>
+    var temps = <?php echo $temps; ?>
+    var humis = <?php echo $humis; ?>
+    var press = <?php echo $press; ?>
+    $("#d_temp").html(temps[0]);
+    $("#d_humi").html(humis[0]);
+    $("#d_pres").html(press[0]);
+</script>
+<h1>BME280 温度・湿度・気圧</h1>
+<h2>現在の情報</h2>
+<h3>温度</h3>
+<p class="data" id="d_temp"></p>
+<h3>湿度</h3>
+<p class="data" id="d_humi"></p>
+<h3>気圧</h3>
+<p class="data" id="d_pres"></p>
 </body>
 </html>
